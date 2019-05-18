5bf7abc0bd4315904f4d2bf4123f886b9107fc47
marcusabate <marcus.abate98@gmail.com>
2019-05-18 19:51:55 -0400

    Move all publishers to one thread to use one set of ports

--- ROS/tesse_ros_bridge/scripts/tesse_ros_bridge_2.py
+++ ROS/tesse_ros_bridge/scripts/tesse_ros_bridge_2.py
@@ -22,9 +22,22 @@ class TesseROSWrapper:
         self.self_ip = rospy.get_param('~self_ip', '127.0.0.1')
         self.request_port = rospy.get_param('~request_port', '9000')
         self.receive_port = rospy.get_param('~receive_port', '9001')
+        self.camera_width = rospy.get_param('~camera_width', '480')
+        self.camera_height = rospy.get_param('~camera_height', '320')
+        self.camera_fov = rospy.get_param('~camera_fov', '60')
+        self.scene_id = rospy.get_param('~scene_id', '0')
+
+        self.use_sim = rospy.get_param('/use_sim_time', False)
+
 
         self.env = Env(self.client_ip, self.self_ip,
                         self.request_port, self.receive_port)
+
+        result = self.env.request(SetCameraParametersRequest(self.camera_height, self.camera_width, self.camera_fov))
+        print result
+
+        result = self.env.request(SceneRequest(self.scene_id))
+        print result
 
         self.body_frame = 'body_frame'
         self.world_frame = 'world'
@@ -32,8 +45,8 @@ class TesseROSWrapper:
         self.bridge = CvBridge()
 
         self.imu_rate = 100
-        self.image_rate = 100
-        self.speedup_factor = 5 # for simulated time
+        self.image_rate = 20
+        self.speedup_factor = 20 # for simulated time
 
         self.cameras=[(Camera.RGB_LEFT, Compression.OFF, Channels.THREE),
                  (Camera.RGB_RIGHT, Compression.OFF, Channels.SINGLE),
@@ -49,11 +62,63 @@ class TesseROSWrapper:
         self.imu_pub = rospy.Publisher("/tesse/imu", Imu, queue_size=1)
         self.true_pose_pub = rospy.Publisher("/tesse/true_pose", PoseStamped, queue_size=1)
 
-        self.clock_pub = rospy.Publisher("/clock", Clock, queue_size=1)
+        if self.use_sim:
+            self.clock_pub = rospy.Publisher("/clock", Clock, queue_size=1)
 
-        self.imu_timer = rospy.Timer(rospy.Duration(1.0/self.imu_rate), self.imu_cb)
-        self.image_timer = rospy.Timer(rospy.Duration(1.0/self.image_rate), self.image_cb)
-        self.clock_timer = rospy.Timer(rospy.Duration(1.0/1000.0), self.clock_cb)
+        self.imu_counter = 0
+
+        # self.everything_timer = rospy.Timer(rospy.Duration(1.0/1000.0), self.everything_cb)
+        # self.imu_timer = rospy.Timer(rospy.Duration(1.0/self.imu_rate), self.imu_cb)
+        # self.image_timer = rospy.Timer(rospy.Duration(1.0/self.image_rate), self.image_cb)
+        # self.clock_timer = rospy.Timer(rospy.Duration(1.0/1000.0), self.clock_cb)
+
+        while not rospy.is_shutdown():
+            self.everything_cb(None)
+
+    def everything_cb(self, event):
+        """ For running everything in one loop """
+        publish_factor = int(self.imu_rate/self.image_rate)
+
+        if self.imu_counter >= publish_factor:
+            data_request = DataRequest(self.cameras)
+            data_response = self.env.request(data_request)
+
+            metadata = self.parse_metadata(data_response.data)
+
+            for i in range(len(self.cameras)):
+                img_msg = self.bridge.cv2_to_imgmsg(data_response.images[i], 'bgr8')
+                img_msg.header.stamp = rospy.Time(metadata['time'])
+                self.image_publishers[i].publish(img_msg)
+
+            self.imu_counter = 0
+
+        metadata = self.get_cam_metadata()
+
+        if self.use_sim:
+            sim_time = rospy.Time.from_sec(metadata['time'] / self.speedup_factor)
+            self.clock_pub.publish(sim_time)
+
+        imu = Imu()
+        imu.header.stamp = rospy.Time(metadata['time'])
+        imu.header.frame_id = self.body_frame
+
+        imu.orientation.x = metadata['quaternion'][0]
+        imu.orientation.y = metadata['quaternion'][1]
+        imu.orientation.z = metadata['quaternion'][2]
+        imu.orientation.w = metadata['quaternion'][3]
+
+        imu.angular_velocity.x = metadata['ang_vel'][0]
+        imu.angular_velocity.y = metadata['ang_vel'][1]
+        imu.angular_velocity.z = metadata['ang_vel'][2]
+
+        imu.linear_acceleration.x = metadata['ang_vel'][0]
+        imu.linear_acceleration.y = metadata['ang_vel'][1]
+        imu.linear_acceleration.z = metadata['ang_vel'][2]
+
+        self.imu_pub.publish(imu)
+        self.publish_true_pose(metadata)
+
+        self.imu_counter += 1
 
     def imu_cb(self, event):
         """ Publish IMU updates from simulator to ROS """
