cfe6113db1e70028227de52f36cedc9e96e8e1b7
Toni Rosinol <tonirosinol@hotmail.com>
2019-05-18 16:22:44 -0400

    Publish images to ROS

--- ROS/tesse_ros_bridge/scripts/tesse_ros_bridge.py
+++ ROS/tesse_ros_bridge/scripts/tesse_ros_bridge.py
@@ -24,21 +24,36 @@ class Params():
     self.receive_port = rospy.get_param('~receive_port', '9001')
 
 class ImagePublisher:
-  def __init__(self, params):
+  def __init__(self, cameras):
     # Store params
-    self.params = params
+    self.cameras = cameras
 
-    # Setup ROS interface
-    self.image_pub = rospy.Publisher("image_out", Image, queue_size=10)
+    # Setup ROS interface: create a ROS image publisher for each camera
+    rgb_left_pub = rospy.Publisher("rgb_left", Image, queue_size=10)
+    rgb_right_pub = rospy.Publisher("rgb_right", Image, queue_size=10)
+    segmentation_pub = rospy.Publisher("segmentation", Image, queue_size=10)
+    depth_pub = rospy.Publisher("depth", Image, queue_size=10)
+
+    self.publishers = [rgb_left_pub, rgb_right_pub, segmentation_pub, depth_pub]
+
+    assert(len(self.cameras) == len(self.publishers))
+
     self.bridge = CvBridge()
 
-  def publishToROS(self, img_msg_header, cv_image):
-    try:
-      img_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
-      img_msg.header = img_msg_header
-      self.image_pub.publish(img_msg)
-    except CvBridgeError as e:
-      print(e)
+  def publishToROS(self, img_stamp, cv_images):
+    # cv_images must be in the same order as the publishers!
+    # TODO use a dictionary for Camera, instead of enum, in msgs.py
+    # to avoid this potential issue
+    for i in range(len(cv_images)):
+      try:
+        img_msg = self.bridge.cv2_to_imgmsg(cv_images[i], "bgr8")
+        img_msg.header.stamp = img_stamp
+        if i < len(self.publishers):
+          self.publishers[i].publish(img_msg)
+        else:
+          print('There are more images than ROS publishers.')
+      except CvBridgeError as e:
+        print(e)
 
 def tesse_ros_bridge():
     # Init ROS node, do this before parsing params.
@@ -52,12 +67,17 @@ def tesse_ros_bridge():
     env = Env(params.client_ip, params.self_ip,
               params.request_port, params.receive_port)
 
-    # Setup image publisher to ROS
-    image_publisher = ImagePublisher(params)
+    # Cameras in Unity to query.
+    cameras=[(Camera.RGB_LEFT, Compression.OFF, Channels.THREE),
+             (Camera.RGB_RIGHT, Compression.OFF, Channels.SINGLE),
+             (Camera.SEGMENTATION, Compression.OFF, Channels.THREE),
+             (Camera.DEPTH, Compression.OFF, Channels.THREE)]
+
+    # Use common header to all images
+    image_publisher = ImagePublisher(cameras)
 
     # Create data request packet
-    data_request = DataRequest(cameras=[(Camera.SEGMENTATION, Compression.OFF, Channels.THREE),
-                                        (Camera.RGB_LEFT, Compression.OFF, Channels.SINGLE)])
+    data_request = DataRequest(cameras);
 
     rate = rospy.Rate(60) # In Hz
     while not rospy.is_shutdown():
@@ -68,13 +88,12 @@ def tesse_ros_bridge():
       data_response = env.request(data_request)
       print(data_response.data)
       imgs = data_response.images
-      for i in range(len(imgs)):
-        cv2.imshow("Image with Id: %s"%i, imgs[i])
+      print len(imgs)
 
       # Publish images to ROS
+      image_publisher.publishToROS(rospy.Time.now(), imgs)
 
-      # Wait to visualize image
-      cv2.waitKey(1)
+      # Wait to keep desired frames per second.
       rate.sleep()
 
 if __name__ == '__main__':
