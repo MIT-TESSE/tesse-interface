592ba1ca9020fd7f2c3993e0a9f552e3cee366dd
mabate <mabate@mit.edu>
2019-08-15 13:03:28 -0400

    Add rate-limiting to UdpListener

--- python/src/tesse/utils.py
+++ python/src/tesse/utils.py
@@ -1,10 +1,30 @@
 import socket
 import threading
-
+import time
 
 class UdpListener(threading.Thread):
-    def __init__(self, host=None, port=None, timeout=0.1):
+    def __init__(self, host='<broadcast>', port=None, timeout=0.01, rate=None):
+        """Initializer for UdpListener object.
+
+            Derived from a threading.Thread object, UdpListener will spin off
+            a new thread and listen for data coming in on a specified port.
+            It supports handing that data with an arbitrary number and type
+            of custom callback functions.
+
+            Args:
+                host: A string representing the hostname which will be used
+                    to listen for data. Default is '<broadcast>', which will
+                    iterate over all network interfaces for send.
+                port: An integer representing the port number to listen to.
+                    This must be set by the user and has no valid default.
+                timeout: A float representing the timeout period for the socket.
+                    Default is 0.1 seconds.
+                rate: A float representing the desired rate at which to call
+                    all custom callbacks on the incoming data. Default is None,
+                    which will send data to the callbacks as fast as possible.
+        """
         super(UdpListener, self).__init__()
+        self.rate = rate
 
         # get ip address
         if host is None:
@@ -25,18 +45,39 @@ class UdpListener(threading.Thread):
         self.handlers = {}
 
     def subscribe(self, name, handler):
+        """Adds custom callback function to dictionary of subscribers.
+
+            Args:
+                name: A string representing the unique name of the callback.
+                handler: A function or class (with __call__ implemented)
+                    to use as a callback when data is received.
+        """
         self.handlers[name] = handler
 
     def join(self, timeout=1):
+        """Safely close thread and unbind socket."""
         self.alive.clear()
         threading.Thread.join(self, timeout)
         self.sock.close()
 
     def run(self):
+        """Main loop for receiving data and calling callbacks."""
+        start = time.time()
         while self.alive.isSet():
             try:
                 data = self.sock.recv(1024)
-                for name in self.handlers:
-                    self.handlers[name](data)
-            except socket.timeout:
-                pass
+            except socket.timeout as error:
+                print "UdpListener error: ", error
+                continue
+
+            for name in self.handlers:
+                self.handlers[name](data)
+
+            if self.rate is not None:
+                while (1.0/self.rate) - (time.time()-start) > 0:
+                    try:
+                        _ = self.sock.recv(1024)
+                    except socket.timeout:
+                        print "UdpListener error: inner timeout"
+                        continue
+                start = time.time()
