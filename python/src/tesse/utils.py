a97b4f5173c3c55fb9b0225f7d3bcfce56bb8dce
Dan Griffith <dan.griffith@ll.mit.edu>
2019-08-27 10:33:58 -0400

    Added ability to throttle rate of metadata messages.

--- python/src/tesse/utils.py
+++ python/src/tesse/utils.py
@@ -1,9 +1,13 @@
 import socket
 import threading
+import defusedxml.ElementTree as ET
 
 
 class UdpListener(threading.Thread):
-    def __init__(self, host=None, port=None, timeout=0.1):
+    __epsilon_timing__ = 0.0025 # 1/(2*200), the game runs at ~200 Hz but sometimes the timing is a little off.
+                                # We use this epsilon to approximate that 1/rate time has elapsed in the game.
+
+    def __init__(self, host=None, port=None, timeout=0.1, rate=None):
         super(UdpListener, self).__init__()
 
         # get ip address
@@ -22,6 +26,9 @@ class UdpListener(threading.Thread):
         self.alive = threading.Event()
         self.alive.set()
 
+        # Desired rate that messages are processed
+        self.rate = rate
+
         self.handlers = {}
 
     def subscribe(self, name, handler):
@@ -33,10 +40,17 @@ class UdpListener(threading.Thread):
         self.sock.close()
 
     def run(self):
+        last_game_time = -1e6  # initialize
         while self.alive.isSet():
             try:
                 data = self.sock.recv(1024)
+            except socket.timeout as error:
+                print("UdpListener error: ", error)
+                continue
+
+            # Process the message if the game has elapsed *at least* 1/self.rate
+            game_time = float(ET.fromstring(data.decode('utf-8')).find('time').text)
+            if self.rate is None or game_time - last_game_time >= 1.0/self.rate - self.__epsilon_timing__:
                 for name in self.handlers:
                     self.handlers[name](data)
-            except socket.timeout:
-                pass
+                last_game_time = game_time
