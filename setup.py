816541f3cc94075022b5ea9801b8e19cd6ac0a4d
griffith826 <j.dan.griffith@gmail.com>
2020-02-26 16:21:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tesse',
-    version='0.1.1',
+    version='0.1.2+snapshot',
     description='TESSE python interface',
     packages=find_packages('src'),
     # tell setuptools that all packages will be under the 'src' directory
